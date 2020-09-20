aa41da032a129fc26a61487865a36abd158afbab
bharathjinka09 <bharathjinka09@gmail.com>
2020-09-20 14:20:03 +0530

    Updates

---
+++ precache-manifest.b1ec231d31c592ee89834dd60630ac1b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a294929aed0a577f785e00619b4b00f8",
+    "url": "/react-codepen-clone/index.html"
+  },
+  {
+    "revision": "70c3ff1a8dd6beff5ca9",
+    "url": "/react-codepen-clone/static/css/2.c46397a6.chunk.css"
+  },
+  {
+    "revision": "3412b2a29a3e2a59bc7c",
+    "url": "/react-codepen-clone/static/css/main.7d2118c4.chunk.css"
+  },
+  {
+    "revision": "70c3ff1a8dd6beff5ca9",
+    "url": "/react-codepen-clone/static/js/2.4254f81d.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-codepen-clone/static/js/2.4254f81d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3412b2a29a3e2a59bc7c",
+    "url": "/react-codepen-clone/static/js/main.1fe89a35.chunk.js"
+  },
+  {
+    "revision": "583799b05e88ee4ecca8",
+    "url": "/react-codepen-clone/static/js/runtime-main.1fe374de.js"
+  }
+]);
